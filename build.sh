6f9d3baba7f4948b89144b0347519b0c1a8e1fcd
Sachin Kori <sachinkori9324@gmail.com>
2026-08-28 17:06:41 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -o errexit
 
-apt-get update && apt-get install -y tesseract-ocr
+# Update and install tesseract on debian-based linux instances
+if command -v apt-get &> /dev/null; then
+    apt-get update && apt-get install -y tesseract-ocr || true
+fi
+
 pip install --upgrade pip
 pip install -r requirements.txt
